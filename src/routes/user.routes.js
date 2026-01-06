d40f45b53964f4a2c5a166878b0271bbbba661ed
Jayant Lalwani <lalwanij32@gmail.com>
2026-01-07 00:56:00 +0530

    add refresh accesstoken functionality with JWT verification in user controller and routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import registerUser from "../controllers/user.controller.js";
 const router = Router();
 import { upload } from "../middlewares/multer.middleware.js";
-import {loginUser, logoutUser} from "../controllers/user.controller.js";
+import { loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
 
 router.route("/register").post(
     upload.fields([
@@ -15,5 +15,6 @@ router.route("/login").post(loginUser);
 
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;// learn about express.Router()
