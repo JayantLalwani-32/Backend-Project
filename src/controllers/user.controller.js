07350a422895f7b575e9a0c7b04e710e19e99f1e
Jayant Lalwani <lalwanij32@gmail.com>
2026-01-08 18:16:26 +0530

    add getUserChannelProfile functionality to fetch user channel details with subscriber counts

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -316,11 +316,83 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, { user }, "User cpver image updated successfully"));
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+    
+    if (!username?.trim()) {
+        throw new ApiError(400, "Username is missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: { username: username?.toLowerCase() } // gives the user document of User model of that username to next stage of pipeline
+        },
+        {
+            $lookup: {
+                from: "subscribers", // collection name in db
+                localField: "_id", // field from User model
+                foreignField: "channel", // field from Subscriber model
+                as: "subscribers" // new field to be added to user document
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions", // collection name in db
+                localField: "_id", // field from User model
+                foreignField: "subscriber", // field from Subscription model
+                as: "subscribedTo" // new field to be added to user document
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscriberCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+
+    ])
+
+    // console log channel to see the structure after aggregation
+    if (!channel?.length) {
+        throw new ApiError(404, "Channel not found");
+    }
+
+    return res
+        .status(200)
+        .json(
+        new ApiResponse(200, channel[0], "User channel profile fetched successfully")
+    )   
+})
+
 export default registerUser;
 export {
     loginUser,
     logoutUser,
     refreshAccessToken, getCurrentUser, changeCurrentPassword, updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile
 };
